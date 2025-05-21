aaed73c8605a93d7a0cd26b62e7d1e8d334dabd6
octavio gutierrez <perez.suareza1@gmail.com>
2025-05-21 10:42:22 -0500

    creacion de vista cargando
    cree la vista cargando

---
+++ angel/lib/feature/home/presntation/view/vista_cargando.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+Scaffold cargando() {
+  return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Centra verticalmente
+          crossAxisAlignment: CrossAxisAlignment.center, // Centra horizontalmente
+          children: [
+            SizedBox(
+              width: 200, // Ajusta el tamaño de la imagen
+              height: 200, // Ajusta el tamaño de la imagen
+              child: Image.network(
+                  "https://cdn.pixabay.com/animation/2023/08/11/21/18/21-18-05-265_512.gif"),
+            ),
+            // Espacio entre imagen y texto
+            Text("Cargando..."),
+          ],
+        ),
+      ),
+    );
+}
