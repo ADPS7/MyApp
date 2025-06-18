f4b3da1ba816bc9987139cf01df418f75b63dfb1
saimeth <diazsaimeth@gmail.com>
2025-06-18 11:07:57 -0500

    algunos ajustes de la vista loading

--- angel/lib/feature/home/presntation/view/vista_cargando.dart
+++ angel/lib/feature/home/presntation/view/vista_cargando.dart
@@ -9,7 +9,19 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Center(
+     body: Stack(
+    fit: StackFit.expand,
+    children: [
+      // Fondo con imagen
+      Opacity(
+        opacity: 1,
+        child: Image.network("https://i.pinimg.com/1200x/10/8a/5d/108a5d3d6c5f0e71ab2faaa0d3fa9a52.jpg",
+          fit: BoxFit.cover,
+        ),
+      ),
+    
+    
+     Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center, // Centra verticalmente
         crossAxisAlignment: CrossAxisAlignment.center, // Centra horizontalmente
@@ -18,13 +30,16 @@ class Loading extends StatelessWidget {
             width: 200, // Ajusta el tamaño de la imagen
             height: 200, // Ajusta el tamaño de la imagen
             child: Image.network(
-                "https://cdn.pixabay.com/animation/2023/08/11/21/18/21-18-05-265_512.gif"),
+                "https://i.postimg.cc/N0QZy69C/Tenor-unscreen.gif"),
           ),
           // Espacio entre imagen y texto
           Text("Cargando..."),
         ],
       ),
     ),
+    ]
+    )
    );
   }
 }
+
